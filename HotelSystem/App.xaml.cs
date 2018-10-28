1b9918c6e6d04fc91f0f46974446bc8abd955506
malcolm.mcneely.86 <malcolm.mcneely.86@gmail.com>
2018-10-28 12:15:31 +0000

    Integrated PRISM with DryIoc

--- HotelSystem/App.xaml.cs
+++ HotelSystem/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Prism.Ioc;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -11,7 +12,16 @@ namespace HotelSystem
    /// <summary>
    /// Interaction logic for App.xaml
    /// </summary>
-   public partial class App : Application
+   internal partial class App : PrismApplication
    {
+      //protected override void RegisterTypes(IContainerRegistry containerRegistry)
+      //{
+      //   throw new NotImplementedException();
+      //}
+
+      //protected override Window CreateShell()
+      //{
+      //   throw new NotImplementedException();
+      //}
    }
 }
---
+++ HotelSystem/PrismApplication.cs
@@ -0,0 +1,52 @@
+﻿using CommonServiceLocator;
+using DryIoc;
+using HotelSystem.Ioc;
+using Prism;
+using Prism.Ioc;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HotelSystem
+{
+   public class PrismApplication : PrismApplicationBase
+   {
+      /// <summary>
+      /// Create <see cref="Rules" /> to alter behavior of <see cref="IContainer" />
+      /// </summary>
+      /// <returns>An instance of <see cref="Rules" /></returns>
+      protected virtual Rules CreateContainerRules() => Rules.Default.WithAutoConcreteTypeResolution()
+                                                                     .With(Made.Of(FactoryMethod.ConstructorWithResolvableArguments))
+                                                                     .WithDefaultIfAlreadyRegistered(IfAlreadyRegistered.Replace);
+
+      protected override IContainerExtension CreateContainerExtension()
+      {
+         return new DryIocContainerExtension(new Container(CreateContainerRules()));
+      }
+
+      protected override Window CreateShell()
+      {
+         return new Shell();
+         //return Container.Resolve<Shell>();
+      }
+
+      protected override void RegisterRequiredTypes(IContainerRegistry containerRegistry)
+      {
+         base.RegisterRequiredTypes(containerRegistry);
+
+         containerRegistry.RegisterSingleton<IRegionNavigationContentLoader, RegionNavigationContentLoader>();
+         containerRegistry.RegisterSingleton<IServiceLocator, DryIocServiceLocatorAdapter>();
+      }
+
+      protected override void RegisterTypes(IContainerRegistry containerRegistry)
+      {
+         base.RegisterFrameworkExceptionTypes();
+
+         ExceptionExtensions.RegisterFrameworkExceptionType(typeof(ContainerException));
+      }
+   }
+}
