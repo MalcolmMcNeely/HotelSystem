c39e89536596a3dfce436dd89b309f5445712646
malcolm.mcneely.86 <malcolm.mcneely.86@gmail.com>
2018-11-04 21:42:26 +0000

    Introduced UserControls for different types of labelled text boxes and common style file

---
+++ HotelSystem.Infrastructure/UserControls/LabelledIntegerTextBox.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HotelSystem.Infrastructure.UserControls
+{
+   /// <summary>
+   /// Interaction logic for LabelledIntegerTextBox.xaml
+   /// </summary>
+   public partial class LabelledIntegerTextBox : UserControl
+   {
+      public LabelledIntegerTextBox()
+      {
+         InitializeComponent();
+      }
+   }
+}
