ac1109b49fd1410d5b98fe2e1fd19b3d72a7f046
malcolm.mcneely.86 <malcolm.mcneely.86@gmail.com>
2018-11-04 14:13:19 +0000

    Implemented Regions in the Shell

--- NavigationToolbar/NavigationModule.cs
+++ NavigationToolbar/NavigationModule.cs
@@ -1,24 +1,35 @@
-﻿using Navigation.Views;
+﻿using HotelSystem.Infrastructure;
+using Navigation.Views;
 using NavigationToolbar.Interfaces;
 using NavigationToolbar.ViewModels;
 using Prism.Ioc;
 using Prism.Modularity;
+using Prism.Regions;
 
 namespace Navigation
 {
    [Module(ModuleName = "NavigationModule")]
    public class NavigationModule : IModule
    {
+      IRegionManager _regionManager;
+
+      public NavigationModule(IRegionManager regionManager)
+      {
+         _regionManager = regionManager;
+      }
+
       // this will give out infra.dryioccontainerextension
       public void OnInitialized(IContainerProvider containerProvider)
       {
-         //throw new NotImplementedException();         
+         //throw new NotImplementedException();
       }
 
       public void RegisterTypes(IContainerRegistry containerRegistry)
       {
          containerRegistry.Register<NavigationView>();
          containerRegistry.Register<INavigationViewModel, NavigationViewModel>();
+
+         _regionManager.RegisterViewWithRegion(RegionNames.NavigationRegion, typeof(NavigationView));
       }
    }
 }
